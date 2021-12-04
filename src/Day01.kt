dbbe5dcd18f033d0f95bd208031ef96044ff3347
Thijs Boehme <thijsboehme@me.com>
2021-12-04 17:52:35 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,44 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        var floor = 0
+        input.forEach {
+            when (it) {
+                '(' -> floor++
+                ')' -> floor--
+                else -> throw IllegalArgumentException()
+            }
+        }
+        return floor
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        var floor = 0
+        input.forEachIndexed { index, c ->
+            when (c) {
+                '(' -> floor++
+                ')' -> floor--
+                else -> throw IllegalArgumentException()
+            }
+            if (floor == -1) return index + 1
+        }
+        throw IllegalStateException()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1("(())") == 0)
+    check(part1("()()") == 0)
+    check(part1("(((") == 3)
+    check(part1("(()(()(") == 3)
+    check(part1("))(((((") == 3)
+    check(part1("())") == -1)
+    check(part1("))(") == -1)
+    check(part1(")))") == -3)
+    check(part1(")())())") == -3)
 
-    val input = readInput("Day01")
+    check(part2(")") == 1)
+    check(part2("()())") == 5)
+
+    val input = readInput("Day01").first()
     println(part1(input))
     println(part2(input))
 }
